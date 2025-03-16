08f537d664214b6060ee02a61746c2a4ca5ccfff
phphe <phphe@outlook.com>
2025-03-16 12:19:51 +0800

    feat: use unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/fonts'
-  ]
-})
+    "@unocss/nuxt",
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/fonts",
+  ],
+});
